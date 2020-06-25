b0d6d9aa3a5ebef88cd6b06fe6a85464136807db
Christopher Frenning <christopher@frenning.com>
2020-06-25 19:11:16 +0000

    Some todo's that must be remembered

--- setup.sh
+++ setup.sh
@@ -3,4 +3,11 @@
 # Setup script for ironviper
 # TODO: Clone Git Repository and do actual work
 
-echo "Not quite there yet, try again in a few cycles"
+echo "Not quite there yet, try again in a few cycles..."
+
+sudo chmod +x ./infrastructure/deploy_azure.sh
+./infrastructure/deploy_azure.sh
+
+# TODO: Write from configuration to /frontend/local.settings.json for easier debugging, we need instancename and storagekey
+
+echo "Done. See new resource group: $rng"
