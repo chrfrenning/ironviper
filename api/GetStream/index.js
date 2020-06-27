2a40e4d0dd7e0f3984116d76cdc7f0ae8b1654b0
Christopher Frenning <christopher@frenning.com>
2020-06-27 11:12:47 +0000

    api scaffold

---
+++ api/GetStream/index.js
@@ -0,0 +1,31 @@
+module.exports = function (context, req) {
+    console.log('Querying table service');
+
+    // load configuration stuff we need
+
+    var account = process.env["InstanceName"];
+    var accountKey = process.env["StorageAccountKey"];
+
+    // query azure table storage
+
+    client = storage.createTableService('DefaultEndpointsProtocol=https;AccountName=' + account + ';AccountKey=' + accountKey + ';EndpointSuffix=core.windows.net');
+    
+    client.retrieveEntity('files', '', 'DSCF8458', function(e,r) { 
+        console.log("Query completed.");
+        
+        if ( !e )
+        {
+            context.res = { 
+                body: r 
+            };
+            context.done();
+        }
+        else {
+            context.res = {
+                status: 400,
+                body: "Please pass a name on the query string or in the request body"
+            };
+            context.done();
+        }
+    });
+};
