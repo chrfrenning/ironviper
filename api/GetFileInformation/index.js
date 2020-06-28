7b1d086a5e701f91d37850bb02e0916986e54eb5
Christopher Frenning <christopher@frenning.com>
2020-06-28 11:08:29 +0000

    fixed stuff; now working getfileinfo

--- api/GetFileInformation/index.js
+++ api/GetFileInformation/index.js
@@ -1,17 +1,23 @@
-module.exports = function (context, req) {
-    console.log('Querying table service');
+var storage = require('azure-storage')
 
-    // load configuration stuff we need
+module.exports = function (context, req) {
+    console.log('Direct lookup of individual file information');
 
     var account = process.env["InstanceName"];
     var accountKey = process.env["StorageAccountKey"];
+
+    var id = req.query.id;
+    
+    var sep = id.indexOf('-')
+    var partition_key = id.slice(0,sep)
+
+    console.log('Id: ' + id + ', partition_key: ' + partition_key)
 
     // query azure table storage
 
     client = storage.createTableService('DefaultEndpointsProtocol=https;AccountName=' + account + ';AccountKey=' + accountKey + ';EndpointSuffix=core.windows.net');
     
-    client.retrieveEntity('files', '', 'DSCF8458', function(e,r) { 
-        console.log("Query completed.");
+    client.retrieveEntity('files', partition_key, id, function(e,r) { 
         
         if ( !e )
         {
@@ -21,11 +27,14 @@ module.exports = function (context, req) {
             context.done();
         }
         else {
+            console.log(e);
+
             context.res = {
                 status: 400,
-                body: "Please pass a name on the query string or in the request body"
+                body: "Resource not found."
             };
             context.done();
         }
+        
     });
 };
