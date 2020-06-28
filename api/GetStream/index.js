6f36abcc35767a53587abd72877b2f64196b289c
Christopher Frenning <christopher@frenning.com>
2020-06-28 11:21:10 +0000

    Returns all entries and all data (!)

--- api/GetStream/index.js
+++ api/GetStream/index.js
@@ -1,36 +1,36 @@
+var storage = require('azure-storage');
+const { TableQuery } = require('azure-storage');
+
 module.exports = function (context, req) {
-    console.log('Querying table service');
+    console.log('Querying table service for latest files');
 
     // load configuration stuff we need
 
     var account = process.env["InstanceName"];
     var accountKey = process.env["StorageAccountKey"];
 
-    var id = req.query.id;
-    var sep = id.find('-')
-    var partition_key = id.split(0,sep)
-    var row_key = id.split(sep+1)
-
     // query azure table storage
 
     client = storage.createTableService('DefaultEndpointsProtocol=https;AccountName=' + account + ';AccountKey=' + accountKey + ';EndpointSuffix=core.windows.net');
     
-    client.retrieveEntity('files', partition_key, row_key, function(e,r) { 
+    client.queryEntities('files', new TableQuery(), null, function(e,r) {
         console.log("Query completed.");
-        
+
         if ( !e )
         {
             context.res = { 
-                body: r 
+                body: r.entries 
             };
             context.done();
         }
         else {
             context.res = {
-                status: 400,
-                body: "Please pass a name on the query string or in the request body"
+                status: 500,
+                body: "Can not query files collection."
             };
             context.done();
         }
-    });
+    } 
+    );
+
 };
