9e51f733471b8ef6796a4e6579bff5fd3f902b96
Christopher Frenning <christopher@frenning.com>
2020-06-29 11:31:27 +0000

    added wiki as submodule for easier dev work

--- converter/run_docker.py
+++ converter/run_docker.py
@@ -5,5 +5,5 @@ configuration = toml.load("../configuration.toml")
 account_name = configuration["instance_name"]
 account_key = configuration["account_key"]
 
-os.system("sudo docker build -t ironviper-converter .")
+os.system("sudo docker build -t ironviper-converter:latest .")
 os.system("sudo docker run -it --rm --env INSTANCE_NAME={} --env ACCOUNT_KEY={} ironviper-converter".format(account_name, account_key))
