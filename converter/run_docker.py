8b85bf175125203a861fb972aaeb70d0792ec461
Christopher Frenning <christopher@frenning.com>
2020-07-30 16:27:11 +0000

    rename account_key to storage_key in conf

--- converter/run_docker.py
+++ converter/run_docker.py
@@ -5,7 +5,7 @@ import os
 configuration_file_name = os.path.dirname(os.path.abspath(__file__)) + "/../configuration.toml"
 configuration = toml.load(configuration_file_name)
 account_name = configuration["instance_name"]
-account_key = configuration["account_key"]
+account_key = configuration["storage_key"]
 
 os.system("sudo docker build -t ironviper-converter:latest .")
 os.system("sudo docker run -it --rm --env INSTANCE_NAME={} --env ACCOUNT_KEY={} ironviper-converter".format(account_name, account_key))
