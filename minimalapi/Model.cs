17340188ed7a0ff4b892dc970280dde6eeb984b4
Christopher Frenning <christopher@frenning.com>
2022-07-09 06:47:05 +0000

    more scaffolding of api

--- minimalapi/Model.cs
+++ minimalapi/Model.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+using Azure;
+using Azure.Data.Tables;
 
 class Folder {
-    // constructor
+    public Folder() {
+        this.Id = String.Empty;
+        this.Name = String.Empty;
+        this.Children = new List<Folder>();
+    }
     public Folder(string name) {
         this.Id = CreateFolderUniqueId();
         this.Name = name;
@@ -22,6 +30,7 @@ class Folder {
     [JsonIgnore] public List<Folder> Children { get; set; }
 
     [JsonIgnore] public Folder? Parent { get; set; }
+
     public Folder GetRoot() {
         Folder current = this;
         while (current.Parent != null) {
@@ -32,18 +41,20 @@ class Folder {
 
     public Folder AddChild(Folder child) {
         child.Parent = this;
-        this.Children.Add(child);
 
         Folder root = this.GetRoot();
-        while(!IsIdUnique(root,child.Id)) {
+        while(!IsIdUnique(root, child.Id)) {
             child.Id = CreateFolderUniqueId();
         }
 
+        this.Children.Add(child);
+        this.Children.Sort((a, b) => a.Name.CompareTo(b.Name));
         return child;
     }
 
     public static string CreateFolderUniqueId() {
-        return IDGenerator.Generate(7);
+        const int FOLDER_ID_LENGTH = 7;
+        return IDGenerator.Generate(FOLDER_ID_LENGTH);
     }
     public static bool IsIdUnique(Folder root, string id) {
         // Check the folder itself
@@ -75,8 +86,14 @@ class Folder {
         return null;
     }
 
-    public static Folder? FindFolderByPath(Folder root, string path) {
-        Folder? node = root;
+    public static Folder? FindFolderByPath(Folder start, string path) {
+        Folder? node = start;
+
+        // check if this is an absolute path, if so start from root
+        if ( path[0] == '/' )
+            node = start.GetRoot();
+
+        // move to the next folder in the path
         foreach ( var p in path.Split('/') ) {
             if (p == "") {
                 continue;
@@ -86,9 +103,165 @@ class Folder {
                 return null;
             }
         }
+
         return node;
     }
+
+    public static Folder CreateFolder(Folder parent, string path) {
+        Debug.Assert(parent != null);
+        Debug.Assert(path != null && path.Length > 0);
+
+        // Check if this is a relative or absolute path and move to root
+        // if absolute
+        if ( path[0] == '/' ) 
+            parent = parent.GetRoot();
+
+        // Split the path and make sure each level exist
+        var parts = path.Split('/');
+        for (int i = 0; i < parts.Length; i++) {
+            var part = parts[i];
+            if (part == "") {
+                continue;
+            }
+            var child = parent.Children.Find(f => f.Name == part);
+            if (child == null) {
+                child = new Folder(part);
+                parent.AddChild(child);
+            }
+            parent = child;
+        }
+
+        return parent; // this is now the new child
+    }
+
+    public static bool CanDeleteFolder(Folder folder) {
+        // cannot delete if it has children
+        if (folder.Children.Count > 0) {
+            return false;
+        }
+        // cannot delete if it is the root
+        if (folder.Parent == null) {
+            return false;
+        }
+        // can delete
+        return true;
+    }
+
+    public static void DeleteFolder(Folder folder) {
+        Debug.Assert(folder != null);
+        Debug.Assert(folder.Parent != null);
+        Debug.Assert(CanDeleteFolder(folder));
+
+        if ( CanDeleteFolder(folder) ) {
+            folder.Parent.Children.Remove(folder);
+        } else {
+            throw new ApplicationException("Cannot delete folder");
+        }
+    }
+
+    public static void DeleteFolder(Folder parent, string path) {
+        Debug.Assert(parent != null);
+        Debug.Assert(path != null && path.Length > 0);
+
+        Folder? folderToDelete = Folder.FindFolderByPath(parent, path);
+        if ( folderToDelete == null ) {
+            throw new ApplicationException("Cannot find folder to delete");
+        } else {
+            DeleteFolder(folderToDelete);
+        }
+    }
+
+    public static Folder LoadModelFromTable(string connectionString) {
+        const string TABLENAME = "tree";
+        const string PARTITION_KEY = "folder";
+
+        // read from azure table
+        var serviceClient = new TableServiceClient(connectionString);
+        var tableClient = serviceClient.GetTableClient(TABLENAME);
+        var results = tableClient.Query<FolderEntity>(filter: $"PartitionKey eq '{PARTITION_KEY}'");
+
+        // the database may be empty, so then just imitate a root folder
+        List<FolderEntity> allfolders = results.ToList<FolderEntity>();
+        if ( allfolders.Count() == 0 ) {
+            Folder newRoot = new Folder("/");
+            // TODO: Insert this into the db
+            return newRoot;
+        }
+
+        // create root, update with info from db
+        Folder root = new Folder("/");
+        FolderEntity? rootEntity = allfolders.Find(f => f.Name == "/");
+        if ( rootEntity != null ) {
+            root.Id = rootEntity.RowKey;
+            allfolders.Remove(rootEntity);
+        }
+
+        // now walk all other folders and add them to the tree
+        AddChildFolders(root, allfolders);
+        Debug.Assert(allfolders.Count() == 0); // we should have placed all folders, rest are orphans and somethings off
+        if ( allfolders.Count() > 0 ) {
+            Folder orphans = root.AddChild( new Folder("_orphans") );
+            allfolders.ForEach( f => orphans.AddChild( new Folder(f.RowKey, f.Name) ) );
+        }
+        return root;
+    }
+
+    static void AddChildFolders(Folder parent, List<FolderEntity> allfolders) {
+        var children = allfolders.FindAll(f => f.Parent == parent.Id);
+        foreach (var child in children) {
+            Folder folder = new Folder(child.RowKey, child.Name);
+            folder.ParentId = child.Parent;
+            parent.AddChild(folder);
+            allfolders.Remove(child);
+            AddChildFolders(folder, allfolders);
+        }
+    }
 };
+
+class FolderEntity : ITableEntity {
+    public FolderEntity() {
+        this.PartitionKey = this.RowKey = String.Empty;
+        this.Timestamp = DateTime.Now;
+        this.ETag = new ETag();
+        this.Name = this.Parent = String.Empty;
+    }
+    public string PartitionKey { get; set; }
+    public string RowKey { get; set; }
+    public DateTimeOffset? Timestamp { get; set; }
+    public ETag ETag { get; set; }
+    public string Parent { get; set; }
+    public string Name { get; set; }
+    // implement tostring for this object
+    public override string ToString() {
+        return $"{this.Name} ({this.RowKey}) {this.Parent}";
+    }
+};
+
+class FileInFolderEntity : ITableEntity {
+    public FileInFolderEntity() {
+        this.PartitionKey = this.RowKey = String.Empty;
+        this.Timestamp = DateTime.Now;
+        this.ETag = new ETag();
+    }
+    public string PartitionKey { get; set; }
+    public string RowKey { get; set; }
+    public DateTimeOffset? Timestamp { get; set; }
+    public ETag ETag { get; set; }
+};
+
+class FileEntity : ITableEntity
+{
+    public FileEntity() {
+        this.PartitionKey = this.RowKey = this.name = String.Empty;
+        this.Timestamp = DateTime.Now;
+        this.ETag = new ETag();
+    }
+    public string PartitionKey { get; set; }
+    public string RowKey { get; set; }
+    public DateTimeOffset? Timestamp { get; set; }
+    public ETag ETag { get; set; }
+    public string name { get; set; }
+}
 
 class File {
     public File(string id, string name) {
@@ -97,4 +270,28 @@ class File {
     }
     public string Id { get; set; }
     public string Name { get; set; }
+
+    public static File? LookupFileById(string id, string connectionString) {
+        const string TABLENAME = "files";
+
+        // check correct form on the id
+        if ( !Regex.IsMatch(id, "^[a-zA-Z0-9]{3}-[a-zA-Z0-9]{7}$") )
+            return null;
+
+        var idParts = id.Split('-');
+
+        // read from azure table
+        var serviceClient = new TableServiceClient(connectionString);
+        var tableClient = serviceClient.GetTableClient(TABLENAME);
+        var results = tableClient.Query<FileEntity>(filter: $"PartitionKey eq '{idParts[0]}' and RowKey eq '{id}'");
+        var file = results.FirstOrDefault();
+        if ( file == null ) 
+            return null;
+
+        return new File(file.RowKey, file.name);
+    }
+    
+    public override string ToString() {
+        return $"{this.Name} ({this.Id})";
+    }
 };
--- minimalapi/Program.cs
+++ minimalapi/Program.cs
@@ -1,60 +1,93 @@
-var tree = CreateTestFolderTree();
+using System.Diagnostics;
+using System.Net.Mime;
+using System.Text;
+
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+var tree = Folder.LoadModelFromTable(app.Configuration["StorageConnectionString"]);
+var file = File.LookupFileById("KK3-zkhZvGn", app.Configuration["StorageConnectionString"]);
+Console.WriteLine("File" + file.Id + file.Name);
 
-app.MapGet("/", () => "IronViper/0.1");
+//TestModel();
+PrintTree( tree );
 
-app.MapGet("/folders", () => {
-    return tree.Children;
-});
-
-app.MapPut("/folders/{name}", (string name) => {
-    Folder root = tree;
-
-    // check if the name is unique
-    Folder? folder = root.Children.Find(f => f.Name == name);
-    if (folder != null) {
-        throw new ApplicationException("Name already exists");
-    }
-    // create a new folder
-    folder = new Folder(name);
-    root.Children.Add(folder);
-    return folder.Id;
-});
-
-app.MapDelete("/folders/{name}", (string name) => {
-    Folder root = tree;
-    Folder? folder = root.Children.Find(f => f.Name == name);
-    if (folder == null) {
-        throw new ApplicationException("Folder not found");
-    }
-    root.Children.Remove(folder);
-    return folder.Id;
+app.MapGet("/", () => {
+    return Results.Ok( new { 
+        application="IronViper/0.1", 
+        version="1.0", 
+        tree="/t/(path)", 
+        nodes="/n/(id)", 
+        edges="/e/(type)", 
+        source="https://github.com/chrfrenning/ironviper/", 
+        copyright="IronViper Software is Copyright (C) Christopher Frenning 2022", 
+        license="MIT",
+        key = app.Configuration["StorageConnectionString"] 
+        });
 });
 
 app.MapGet("/t/", () => {
     return tree.Children;
 });
 
-
 app.MapGet("/t/{*name}", (string name) => {
-    Folder? node = tree;
-
-    foreach( var part in name.Split('/') ) {
-        if (part == "") {
-            continue;
-        }
-
-        node = node.Children.Find(f => f.Name == part);
-
-        if (node == null)
-            return Results.NotFound();
-    }
-
-    return Results.Ok(node.Children);
+    Folder? folder = Folder.FindFolderByPath(tree, name);
+    if ( folder == null )
+        return Results.NotFound();
+    
+    return Results.Ok(folder.Children);
 });
 
-app.MapGet("/i/{id}", (string id) => {
+app.MapPut("/t/{*name}", (string name) => {
+    Folder newFolder = Folder.CreateFolder(tree, name);
+    return Results.Ok(newFolder);
+});
+
+app.MapDelete("/t/{*name}", (string name) => {
+    Folder? folder = Folder.FindFolderByPath(tree, name);
+
+    if ( folder == null )
+        return Results.NotFound();
+
+    if ( !Folder.CanDeleteFolder(folder) )
+        return Results.BadRequest();
+
+    Folder.DeleteFolder(folder);
+    return Results.Ok();
+});
+
+app.MapGet("/tree", () => {
+    StringBuilder sb = new StringBuilder();
+    sb.Append("<html><body><ul>");
+    FolderToHtml(tree, sb);
+    
+    sb.Append("</ul></body></html>");
+
+    return Results.Extensions.Html(sb.ToString());
+
+});
+
+void FolderToHtml(Folder folder, StringBuilder sb) {
+    sb.Append("<li>");
+    sb.Append(folder.Name);
+    sb.Append("<ul>");
+    foreach ( var child in folder.Children ) {
+        FolderToHtml(child, sb);
+    }
+    sb.Append("</ul>");
+    sb.Append("</li>");
+}
+
+app.MapGet("/n/{id:regex(^[a-zA-Z0-9]{{7}}$)}", (string id) => {
+    var folder = Folder.FindFolderById(tree, id);
+
+    if ( folder == null )
+        return Results.NotFound();
+
+    return Results.Ok(folder);
+});
+
+app.MapGet("/n/{id:regex(^[a-zA-Z0-9]{{3}}-[a-zA-Z0-9]{{7}}$)}", (string id) => {
     var folder = Folder.FindFolderById(tree, id);
 
     if ( folder == null )
@@ -73,11 +106,60 @@ Folder CreateTestFolderTree() {
     root.AddChild( new Folder("chris") );
     root.AddChild( new Folder("jacob") );
 
-    Folder julia = root.AddChild( new Folder("julia") );
+    Folder julia = root.AddChild( new Folder("Abcdefg", "julia") );
     julia.AddChild( new Folder("julia-1") );
     julia.AddChild( new Folder("julia-2") );
 
     root.AddChild( new Folder("seb") );
 
     return root;
+}
+
+void TestModel() {
+    Folder root = CreateTestFolderTree();
+    PrintTree( root );
+
+    Debug.Assert( Folder.FindFolderById(root, "Abcdefg") != null );
+    Debug.Assert( Folder.FindFolderByPath(root, "/julia/julia-1") != null );
+
+    Folder folder = Folder.CreateFolder(root, "/julia/julia-2/julia-2-2/julia-2-2-2");
+    PrintTree( root );
+
+    Folder.DeleteFolder(root, "/julia/julia-2/julia-2-2/julia-2-2-2");
+    PrintTree( root );
+    
+}
+
+void PrintTree(Folder root, int level = 0) {
+    Console.WriteLine(new String('-', level) + root.Name + " (" + root.Id + ")");
+    foreach (var child in root.Children) {
+        PrintTree(child, level + 1);
+    }
+}
+
+static class ResultsExtensions
+{
+    public static IResult Html(this IResultExtensions resultExtensions, string html)
+    {
+        ArgumentNullException.ThrowIfNull(resultExtensions);
+
+        return new HtmlResult(html);
+    }
+}
+
+class HtmlResult : IResult
+{
+    private readonly string _html;
+
+    public HtmlResult(string html)
+    {
+        _html = html;
+    }
+
+    public Task ExecuteAsync(HttpContext httpContext)
+    {
+        httpContext.Response.ContentType = MediaTypeNames.Text.Html;
+        httpContext.Response.ContentLength = Encoding.UTF8.GetByteCount(_html);
+        return httpContext.Response.WriteAsync(_html);
+    }
 }
